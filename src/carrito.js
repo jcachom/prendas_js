ac7deca6c8a7e5a43efa3c185508b8d24b1fda1d
jcachom <jcachom10@gmail.com>
2021-07-13 12:22:48 -0500

    carga inicial

---
+++ src/carrito.js
@@ -0,0 +1,167 @@
+ListarCarrito();
+
+$(document).on("click", ".classbtn_delete", function (event) {
+  let id = this.id;
+  let sku = id.split("_")[2];
+
+  Swal.fire({
+    title: "Eliminar",
+    text: "Está seguro de eliminar?",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Aceptar"
+  }).then(function (result) {
+    if (result.value) {
+      $("#tr_" + sku).css("background-color", "#ffca39");
+      $("#tr_" + sku).fadeOut(1000, function () {
+        $("#tr_" + sku).remove();
+
+        actualizarItemCarritoStorage(sku, 0, "ELIMINAR");
+      });
+    }
+  });
+});
+
+$(".classtxt_cantidad").change(function () {
+  let idText = this.id;
+  let arrId = idText.toString().split("_");
+  let sku = arrId[2];
+  let precio = $("#hd_precio_" + sku).val();
+  let qpedido = $("#" + idText).val();
+  if (qpedido === "") {
+    qpedido = 0;
+  }
+  let subTotal = parseFloat(precio) * parseFloat(qpedido);
+  $("#txt_subTotal_" + sku).val(subTotal);
+  actualizarItemCarritoStorage(sku, qpedido, "ADICIONAR");
+  //alert("id seleccionado :" + precio);
+});
+
+function actualizarItemCarritoStorage(sku, qpedido, accion) {
+  let listproductoCarritoJson = localStorage.getItem("listProdCarrito");
+  let listCarritoArray = [];
+  listCarritoArray = JSON.parse(listproductoCarritoJson);
+
+  switch (accion) {
+    case "ADICIONAR":
+      listCarritoArray.forEach((item) => {
+        if (item.sku === sku) {
+          item.cant_pedido = qpedido;
+        }
+      });
+
+      break;
+    case "ELIMINAR":
+      listCarritoArray = listCarritoArray.filter(function (item) {
+        return item.sku !== sku;
+      });
+
+      break;
+
+    default:
+      break;
+  }
+
+  localStorage.setItem("listProdCarrito", JSON.stringify(listCarritoArray));
+
+  ActualizarSubTotales();
+}
+
+function ActualizarSubTotales() {
+  let listproductoCarritoJson = localStorage.getItem("listProdCarrito");
+  let listCarritoArray = [];
+  listCarritoArray = JSON.parse(listproductoCarritoJson);
+  let SubTotalImporte = 0;
+  listCarritoArray.forEach((item) => {
+    SubTotalImporte =
+      SubTotalImporte + parseFloat(item.cant_pedido) * parseFloat(item.precio);
+  });
+  //let impuesto = SubTotalImporte * 0.18;
+  let impuesto = parseFloat(
+    Math.round(SubTotalImporte * 0.18 * 100) / 100
+  ).toFixed(2);
+
+  let totalCobro = parseFloat(SubTotalImporte) + parseFloat(impuesto);
+  $("#txt_subtotal").val(SubTotalImporte);
+  $("#txt_impuesto").val(impuesto);
+  $("#txt_total").val(totalCobro);
+}
+
+function ListarCarrito() {
+  // localStorage.clear();
+  $("#tb_listCarritoBody").html("");
+
+  let listproductoCarritoJson = localStorage.getItem("listProdCarrito");
+  let listCarritoArray = [];
+  //let totProductos = 0;
+  if (listproductoCarritoJson != null) {
+    listCarritoArray = JSON.parse(listproductoCarritoJson);
+
+    let html = "";
+    let botonEliminar = "";
+    let inputCant = "";
+    let txtSubTotal = "";
+    let hdInput = "";
+    for (let item of listCarritoArray) {
+      //  totProductos = totProductos + parseInt(producto.cant_pedido, 10);
+
+      botonEliminar =
+        "<button type='button' id='btn_eliminar_" +
+        item.sku +
+        "' class='btn btn-sm mr-1 btn-outline-danger classbtn_delete'   title='Eliminar' ><i class='fa fa-trash'></i></button>";
+
+      inputCant =
+        "<input type='number' id='txt_eliminar_" +
+        item.sku +
+        "' value=" +
+        item.cant_pedido +
+        " class='form-control col-12 classtxt_cantidad' />";
+
+      txtSubTotal =
+        "<input type='number' id='txt_subTotal_" +
+        item.sku +
+        "' value=" +
+        item.precio * item.cant_pedido +
+        " class='form-control col-12 classtxt_SubTotal' disabled />";
+
+      hdInput =
+        " <input type='hidden' id=hd_precio_" +
+        item.sku +
+        " value='" +
+        item.precio +
+        "'/>";
+
+      html =
+        html +
+        "<tr id='tr_" +
+        item.sku +
+        "' >" +
+        "<td>" +
+        item.sku +
+        "</td>" +
+        "<td>" +
+        item.nombre +
+        "</td>" +
+        "<td>" +
+        //item.cant_pedido +
+        inputCant +
+        "</td>" +
+        "<td>" +
+        item.precio +
+        hdInput +
+        "</td>" +
+        "<td>" +
+        //item.precio * item.cant_pedido +
+        txtSubTotal +
+        "</td>" +
+        "<td>" +
+        botonEliminar +
+        "</td>" +
+        "</tr>";
+    }
+
+    $("#tb_listCarritoBody").html(html);
+    ActualizarSubTotales();
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,569 @@
+﻿/*
+class Producto {
+  constructor(
+    sku,
+    nombre,
+    moneda,
+    precio,
+    nombre_img,
+    tipo,
+    detalle,
+    cant_pedido,
+    linea_pedido
+  ) {
+    this.sku = sku;
+    this.nombre = nombre;
+    this.moneda = moneda;
+    this.precio = precio;
+    this.nombre_img = nombre_img;
+    this.tipo = tipo;
+    this.detalle = detalle;
+    this.cant_pedido = cant_pedido;
+    this.linea_pedido = linea_pedido;
+  }
+}
+
+const lProductosS1 = [];
+//const lProductosS2 = [];
+
+//const lProductos = [];
+
+lProductosS1.push(
+  new Producto(
+    "007",
+    "Gabardina",
+    "$",
+    40,
+    "producto_7.png",
+    "Gabardina de lana de gran oferta para hombre abrigo de invierno cálido de Color liso prendas d YUA",
+    0,
+    0
+  )
+);
+lProductosS1.push(
+  new Producto(
+    "008",
+    "Chompa",
+    "$",
+    50,
+    "producto_8.png",
+    "Chompa Hombre Newport Fz Greg I20-Verde",
+    0,
+    0
+  )
+);
+lProductosS1.push(
+  new Producto(
+    "009",
+    "Abrigo",
+    "$",
+    60,
+    "producto_9.png",
+    "Abrigo Hombre Christian Lacroix Jkt Detanv W20-Negro",
+    0,
+    0
+  )
+);
+lProductosS1.push(
+  new Producto(
+    "010",
+    "Abrigo",
+    "$",
+    70,
+    "producto_10.png",
+    "Abrigo Hombre Basement Jkt Tednv W20-Negro",
+    0,
+    0
+  )
+);
+lProductosS1.push(
+  new Producto(
+    "011",
+    "Abrigo",
+    "$",
+    80,
+    "producto_11.png",
+    "Abrigo de lana corto para hombre abrigo de lana cálido para hombre de mediana edad chaquetas g MIS",
+    0,
+    0
+  )
+);
+lProductosS1.push(
+  new Producto(
+    "012",
+    "ZOGAA",
+    "$",
+    40,
+    "producto_12.png",
+    "ZOGAA, chaquetas y abrigos largos de Otoño Invierno, chaquetas de me =",
+    0,
+    0
+  )
+);
+lProductosS1.push(
+  new Producto(
+    "013",
+    "Abrigo",
+    "$",
+    50,
+    "producto_13.png",
+    "abrigo con cuello de soporte para hombre traje ajustado Chaqueta con botones abrigo blazer Top YUA",
+    0,
+    0
+  )
+);
+lProductosS1.push(
+  new Producto(
+    "014",
+    "Abrigo",
+    "$",
+    60,
+    "producto_14.png",
+    "Abrigo Hombre Christian Lacroix Jkt Detach W20-Marrón",
+    0,
+    0
+  )
+);
+lProductosS1.push(
+  new Producto(
+    "015",
+    "Abrigo",
+    "$",
+    70,
+    "producto_15.png",
+    "abrigo de mezcla de lana de invierno para hombre abrigos de lana para el trabajo de alta calid HON",
+    0,
+    0
+  )
+);
+lProductosS1.push(
+  new Producto(
+    "016",
+    "Abrigo",
+    "$",
+    80,
+    "producto_16.png",
+    "Abrigo de lana a la moda para hombre gabardina larga de Color sólido cálido para invierno abri YUA",
+    0,
+    0
+  )
+);
+
+// inicio dos
+
+lProductosS1.push(
+  new Producto(
+    "017",
+    "Abrigo",
+    "$",
+    90,
+    "producto_17.png",
+    "Abrigo largo grueso con cremallera para hombre abrigos de lana de doble cuello de Color sólido YUA",
+    0,
+    0
+  )
+);
+lProductosS1.push(
+  new Producto(
+    "018",
+    "Laamei",
+    "$",
+    100,
+    "producto_18.png",
+    "Laamei de otoño abrigo de lana de invierno para hombre abrigo de lana para hombre abrigo largo HON",
+    0,
+    0
+  )
+);
+lProductosS1.push(
+  new Producto(
+    "019",
+    "Abrigo",
+    "$",
+    105,
+    "producto_19.png",
+    "Abrigo de lana cálido genuino para hombre abrigo Casual de otoño invierno a cuadros con cuello SAI",
+    0,
+    0
+  )
+);
+lProductosS1.push(
+  new Producto(
+    "020",
+    "Abrigo",
+    "$",
+    110,
+    "producto_20.png",
+    "abrigos de mezcla de lana de invierno para hombre gabardina informal Simple de marca de moda p YUA",
+    0,
+    0
+  )
+);
+lProductosS1.push(
+  new Producto(
+    "021",
+    "Chaqueta",
+    "$",
+    120,
+    "producto_21.png",
+    "Chaqueta de lana de 100% de doble cara para hombre chaqueta con bolsillos grandes con cuello v HON",
+    0,
+    0
+  )
+);
+lProductosS1.push(
+  new Producto(
+    "022",
+    "Abrigo",
+    "$",
+    100,
+    "producto_22.png",
+    "abrigos de mezcla de lana de invierno para hombre gabardina informal Simple de marca de moda p MIS",
+    0,
+    0
+  )
+);
+lProductosS1.push(
+  new Producto(
+    "023",
+    "Abrigo",
+    "$",
+    120,
+    "producto_23.png",
+    "Nuevo alta calidad abrigo de lana suelta gran casuales de los hombres X-Giro largo Collar de d MIS",
+    0,
+    0
+  )
+);
+lProductosS1.push(
+  new Producto(
+    "024",
+    "Chaquetas",
+    "$",
+    130,
+    "producto_24.png",
+    "invierno otoño gruesas chaquetas de lana Abrigos Hombre moda Casual Slim Fit Casual chaquetas SAI",
+    0,
+    0
+  )
+);
+lProductosS1.push(
+  new Producto(
+    "025",
+    "Abrigo",
+    "$",
+    140,
+    "producto_25.png",
+    "Abrigo Casual de lana de doble botonadura para hombre 4XL estilo militar Peacoat hombre Cachem YUA",
+    0,
+    0
+  )
+);
+lProductosS1.push(
+  new Producto(
+    "026",
+    "Abrigo",
+    "$",
+    70,
+    "producto_26.png",
+    "nuevo abrigo de lana de alta calidad para otoño e invierno abrigo largo cortaviento de color s SAI",
+    0,
+    0
+  )
+);
+lProductosS1.push(
+  new Producto(
+    "027",
+    "Abrigo",
+    "$",
+    80,
+    "producto_27.png",
+    "hombres abrigos de la primavera de marca de moda Collar Chaqueta slim para hombre delgado de t YUA",
+    0,
+    0
+  )
+);
+*/
+
+let lProductosS1 = [];
+
+CargarProductos();
+
+function CargarProductos() {
+  const urlJson = "prendas.json";
+  
+
+fetch("prendas.json")
+  .then(response => response.json())
+  .then(json => console.log(json));
+
+ 
+
+
+ 
+ $.ajax({
+            method: "POST",
+            crossDomain: true,
+            dataType: 'jsonp',
+
+
+           body: JSON.stringify(data),
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+                "Accept": 'application/json',
+            }  ;
+
+
+            url:  "https://api.apis.net.pe/v1/tipo-cambio-sunat",         
+            success: function(respuesta){
+                console.log(respuesta);
+            }
+        });
+
+
+
+
+
+  $.getJSON(urlJson, function (respuesta, estado) {
+
+     console.log( "JSON Data: " + respuesta );
+
+    if (estado === "success") {
+      lProductosS1 = respuesta;
+    }
+  });
+
+  let html1 = "";
+  let html2 = "";
+
+  let listVal = "";
+
+  for (let productoS1 of lProductosS1) {
+    if (parseInt(productoS1.sku, 10) <= 16) {
+      html1 = html1 + "<div>";
+      html1 =
+        html1 +
+        " <img src='imagen/" +
+        productoS1.nombre_img +
+        "' alt=''  class='img-fluid img-thumbnail' max-width='100%' height='auto' /> ";
+
+      html1 = html1 + "<div>";
+      html1 = html1 + "<h4>";
+      html1 = html1 + productoS1.nombre;
+      html1 = html1 + "s/." + productoS1.precio;
+      html1 = html1 + "</h4>";
+      html1 = html1 + "</div> ";
+
+      html1 = html1 + "<div>";
+      html1 =
+        html1 +
+        "<button type='button' class='btn btn-danger btn-block classbtn_det' id='btn_" +
+        productoS1.sku +
+        "'>";
+      html1 = html1 + "<i class='fa fa-save'></i> !Lo quiero! </button>";
+      html1 = html1 + "<br>";
+      html1 = html1 + "<br>";
+      html1 = html1 + "</div> ";
+
+      html1 = html1 + "</div> ";
+
+      listVal = "";
+      listVal =
+        listVal +
+        productoS1.sku +
+        "|" +
+        productoS1.nombre +
+        "|" +
+        productoS1.moneda +
+        "|" +
+        productoS1.precio +
+        "|" +
+        productoS1.nombre_img +
+        "|" +
+        productoS1.tipo +
+        "|" +
+        productoS1.detalle;
+      html1 =
+        html1 +
+        " <input type='hidden' id=hd_det_" +
+        productoS1.sku +
+        " value='" +
+        listVal +
+        "'/>";
+    }
+  }
+  document.getElementById("divSeccion1").innerHTML = html1;
+
+  for (let productoS2 of lProductosS1) {
+    if (parseInt(productoS2.sku, 10) >= 17) {
+      html2 = html2 + "<div>";
+      html2 =
+        html2 +
+        " <img src='imagen/" +
+        productoS2.nombre_img +
+        "' alt=''  class='img-fluid img-thumbnail' max-width='100%' height='auto' /> ";
+
+      html2 = html2 + "<div>";
+      html2 = html2 + "<h4>";
+      html2 = html2 + productoS2.nombre;
+      html2 = html2 + "s/." + productoS2.precio;
+      html2 = html2 + "</h4>";
+      html2 = html2 + "</div> ";
+
+      html2 =
+        html2 +
+        "<button type='button' class='btn btn-danger btn-block classbtn_det' id='btn_" +
+        productoS2.sku +
+        "'>";
+      html2 = html2 + "<i class='fa fa-save'></i> !Lo quiero! </button>";
+      html2 = html2 + "<br>";
+      html2 = html2 + "<br>";
+
+      html2 = html2 + "</div> ";
+
+      listVal = "";
+      listVal =
+        listVal +
+        productoS2.sku +
+        "|" +
+        productoS2.nombre +
+        "|" +
+        productoS2.moneda +
+        "|" +
+        productoS2.precio +
+        "|" +
+        productoS2.nombre_img +
+        "|" +
+        productoS2.tipo +
+        "|" +
+        productoS2.detalle;
+      html2 =
+        html2 +
+        " <input type='hidden' id=hd_det_" +
+        productoS2.sku +
+        " value='" +
+        listVal +
+        "'/>";
+    }
+  }
+  document.getElementById("divSeccion2").innerHTML = html2;
+
+  //localStorage.clear();
+}
+
+var classBtn = document.getElementsByClassName("classbtn_det");
+let i = 0;
+for (i = 0; i < classBtn.length; i++) {
+  classBtn[i].addEventListener("click", mostrarDetalleClick);
+}
+
+document.getElementById("divPrevisualizacion").style.display = "none";
+function mostrarDetalleClick() {
+  document.getElementById("divPrevisualizacion").style.display = "block";
+
+  var arrbtnId = this.id.split("_");
+  var codprod = "hd_det_" + arrbtnId[1];
+  var det_prod = document.getElementById(codprod).value.split("|");
+
+  document.getElementById("txt_sku").value = det_prod[0];
+  document.getElementById("txt_producto").value =
+    det_prod[1] + " : " + det_prod[5];
+  document.getElementById("txt_precio").value = det_prod[3];
+
+  document.getElementById("txt_cantidad").value = "";
+
+  document.getElementById("imgVista").src = "imagen/" + det_prod[4];
+
+  let hdivHeader = document.getElementById("divHeader").clientHeight;
+  let hnavHeader = document.getElementById("navHeader").clientHeight;
+  let hcarousel = document.getElementById("carouselExampleIndicators")
+    .clientHeight;
+
+  window.scrollTo(0, hdivHeader + hnavHeader + hcarousel);
+
+  // document.getElementById("sesionUsuario").value;
+  // alert("hizo click:" + arrbtnId);
+}
+
+VisualizarCantidProductoCarrito();
+
+document.getElementById("btn_solicitar").addEventListener("click", function () {
+  let listproductoCarritoJson = localStorage.getItem("listProdCarrito");
+
+  let qpedido = 0;
+  let sku = document.getElementById("txt_sku").value;
+  qpedido = document.getElementById("txt_cantidad").value;
+
+  if (sku === "") {
+    Swal.fire("Seleccione producto.");
+    return false;
+  }
+
+  if (qpedido === "0" || qpedido === "") {
+    Swal.fire("Ingrese cantidad pedido.");
+
+    return false;
+  }
+
+  let ofindSku = lProductosS1.find((p) => p.sku === sku);
+
+  let encontradoCarrito = false;
+  if (ofindSku != null) {
+    let listCarritoArray = [];
+    if (listproductoCarritoJson != null) {
+      listCarritoArray = JSON.parse(listproductoCarritoJson);
+
+      listCarritoArray.forEach((item) => {
+        if (item.sku === sku) {
+          item.cant_pedido =
+            parseInt(item.cant_pedido, 10) + parseInt(qpedido, 10);
+          encontradoCarrito = true;
+        }
+      });
+    }
+
+    if (encontradoCarrito === false) {
+      ofindSku.linea_pedido = LineaPedido(listCarritoArray) + 1;
+      ofindSku.cant_pedido = qpedido;
+      listCarritoArray.push(ofindSku);
+    }
+
+    localStorage.setItem("listProdCarrito", JSON.stringify(listCarritoArray));
+
+    Swal.fire(
+      "Producto adicionado al carrito. Cant. productos:" +
+        listCarritoArray.length
+    );
+    document.getElementById("divPrevisualizacion").style.display = "none";
+
+    VisualizarCantidProductoCarrito();
+  }
+});
+
+function LineaPedido(listProd) {
+  let max = 0;
+  for (let producto of listProd) {
+    if (producto.linea_pedido > max) {
+      max = producto.linea_pedido;
+    }
+  }
+  return max;
+}
+
+function VisualizarCantidProductoCarrito() {
+  // localStorage.clear();
+
+  let listproductoCarritoJson = localStorage.getItem("listProdCarrito");
+  let listCarritoArray = [];
+  let totProductos = 0;
+  if (listproductoCarritoJson != null) {
+    listCarritoArray = JSON.parse(listproductoCarritoJson);
+
+    for (let producto of listCarritoArray) {
+      totProductos = totProductos + parseInt(producto.cant_pedido, 10);
+    }
+  }
+
+  document.getElementById("divCantCarrito").innerHTML = totProductos;
+}
